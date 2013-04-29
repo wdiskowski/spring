3338f619cd8dfa94e144da075a487d8c26eea804
wdiskowski <wdiskowski@gmail.com>
2013-04-29 19:51:18 +0200

    test conflict

--- spring_mvc_security/src/main/java/de/modulware/controller/HomeController.java
+++ spring_mvc_security/src/main/java/de/modulware/controller/HomeController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
- * Sample controller for going to the home page with a message
+ * Sample controller for going to the home page with a message1
  */
 @Controller
 public class HomeController {
